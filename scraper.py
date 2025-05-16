87c0fe670687c8411a136178baef9be3e12e426f
Mohamad Al Ayoubi <127571560+mohammadayyoubi@users.noreply.github.com>
2025-05-17 01:43:02 +0300

    Adding notes on scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ response = requests.get(url)
 # Step 2: Parse HTML content
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Step 3: Extract data
+# Step 3: Extract data -- 7a tjib kl quotes div
 quotes = soup.find_all('div', class_='quote')
 
 # Prepare CSV
@@ -17,11 +17,13 @@ with open('output.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(['Quote', 'Author', 'Link'])
 
+    #loop to path 3la each quote extacted
     for quote in quotes:
-        text = quote.find('span', class_='text').get_text(strip=True)
-        author = quote.find('small', class_='author').get_text(strip=True)
-        link = quote.find('a')['href']
-        full_link = url + link
+        text = quote.find('span', class_='text').get_text(strip=True)  # 7a tfut 3la div qoute b3den el span yle 3ndo class text (howe el qoute text)
+        author = quote.find('small', class_='author').get_text(strip=True) # same, enter small element that have class author
+        link = quote.find('a')['href'] # 7a tfut 3la a element w t7ot el href link
+        full_link = url + link #complete path of link of quote
+        # Step 4: Write to CSV
         writer.writerow([text, author, full_link])
 
 print("Scraping complete. Data saved to output.csv.")
